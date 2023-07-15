076ed7280f7c4bff4347a08acf9a97780ca51b01
Odo_Peter_Ebere <odopeter33@gmail.com>
2023-07-15 09:47:07 -0700

    modified the console file

--- console.py
+++ console.py
@@ -5,12 +5,28 @@ used for CRUD operations
 """
 
 import cmd
+from models import storage
+from models.base_model import  BaseModel
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 
 
 class HBNBCommand(cmd.Cmd):
     """The command shell processor"""
 
     prompt = "(hbnb) "
+
+    def help_help(self):
+        """Help command describes a specific cmd"""
+        print("Describes the function of a given command")
+
+    def emptyline(self):
+        """Does nothing when an empty line is encountered"""
+        pass
 
     def do_quit(self, line):
         """Quit command to exit program"""
